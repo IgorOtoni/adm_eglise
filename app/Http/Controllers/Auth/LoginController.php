e127c40d52f415336bf1b8a810b086e9378694bf
BlackSwitch09 <paulinhohenrique90@yahoo.com.br>
2019-03-21 13:52:51 -0300

    wdw

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/igrejas';
 
     /**
      * Create a new controller instance.
