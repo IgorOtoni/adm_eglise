b2da54928efef223971511d9ad5e30f22a71427e
IgorOtoni <31601703+IgorOtoni@users.noreply.github.com>
2019-04-30 09:24:00 -0300

    Octogésimo primeiro commit.

--- database/seeds/Tbl_Perfis_Permissoes_Seeder.php
+++ database/seeds/Tbl_Perfis_Permissoes_Seeder.php
@@ -19,18 +19,156 @@ class Tbl_Perfis_Permissoes_Seeder extends Seeder
         DB::table('tbl_perfis_permissoes')->insert([
             'id'=>2,
             'id_perfil_igreja_modulo'=>1, // pastor - Igreja 3 - Usuarios
-            'id_permissao'=>2, // incluir registro
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>3,
+            'id_perfil_igreja_modulo'=>1, // pastor - Igreja 3 - Usuarios
+            'id_permissao'=>3, // alterar registro
         ]);
 
         DB::table('tbl_perfis_permissoes')->insert([
-            'id'=>3,
-            'id_perfil_igreja_modulo'=>2, // pastor - Igreja 3 - Perfis
-            'id_permissao'=>2, // incluir registro
-        ]);
-        DB::table('tbl_perfis_permissoes')->insert([
             'id'=>4,
             'id_perfil_igreja_modulo'=>2, // pastor - Igreja 3 - Perfis
-            'id_permissao'=>3, // incluir registro
+            'id_permissao'=>1, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>5,
+            'id_perfil_igreja_modulo'=>2, // pastor - Igreja 3 - Perfis
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>6,
+            'id_perfil_igreja_modulo'=>2, // pastor - Igreja 3 - Perfis
+            'id_permissao'=>3, // desativar registro
+        ]);
+
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>7,
+            'id_perfil_igreja_modulo'=>3, // pastor - Igreja 3 - Publicacoes
+            'id_permissao'=>1, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>8,
+            'id_perfil_igreja_modulo'=>3, // pastor - Igreja 3 - Publicacoes
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>9,
+            'id_perfil_igreja_modulo'=>3, // pastor - Igreja 3 - Publicacoes
+            'id_permissao'=>3, // desativar registro
+        ]);
+
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>10,
+            'id_perfil_igreja_modulo'=>4, // pastor - Igreja 3 - Sermoes
+            'id_permissao'=>1, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>11,
+            'id_perfil_igreja_modulo'=>4, // pastor - Igreja 3 - Sermoes
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>12,
+            'id_perfil_igreja_modulo'=>4, // pastor - Igreja 3 - Sermoes
+            'id_permissao'=>3, // desativar registro
+        ]);
+
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>13,
+            'id_perfil_igreja_modulo'=>5, // pastor - Igreja 3 - Banners
+            'id_permissao'=>1, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>14,
+            'id_perfil_igreja_modulo'=>5, // pastor - Igreja 3 - Banners
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>15,
+            'id_perfil_igreja_modulo'=>5, // pastor - Igreja 3 - Banners
+            'id_permissao'=>3, // desativar registro
+        ]);
+
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>16,
+            'id_perfil_igreja_modulo'=>6, // pastor - Igreja 3 - Noticia
+            'id_permissao'=>1, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>17,
+            'id_perfil_igreja_modulo'=>6, // pastor - Igreja 3 - Noticia
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>18,
+            'id_perfil_igreja_modulo'=>6, // pastor - Igreja 3 - Noticia
+            'id_permissao'=>3, // desativar registro
+        ]);
+
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>19,
+            'id_perfil_igreja_modulo'=>7, // pastor - Igreja 3 - Eventos
+            'id_permissao'=>1, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>20,
+            'id_perfil_igreja_modulo'=>7, // pastor - Igreja 3 - Eventos
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>21,
+            'id_perfil_igreja_modulo'=>7, // pastor - Igreja 3 - Eventos
+            'id_permissao'=>3, // desativar registro
+        ]);
+
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>22,
+            'id_perfil_igreja_modulo'=>8, // pastor - Igreja 3 - Eventos Fixos
+            'id_permissao'=>1, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>23,
+            'id_perfil_igreja_modulo'=>8, // pastor - Igreja 3 - Eventos Fixos
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>24,
+            'id_perfil_igreja_modulo'=>8, // pastor - Igreja 3 - Eventos Fixos
+            'id_permissao'=>3, // desativar registro
+        ]);
+
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>25,
+            'id_perfil_igreja_modulo'=>9, // pastor - Igreja 3 - Galerias
+            'id_permissao'=>1, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>26,
+            'id_perfil_igreja_modulo'=>9, // pastor - Igreja 3 - Galerias
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>27,
+            'id_perfil_igreja_modulo'=>9, // pastor - Igreja 3 - Galerias
+            'id_permissao'=>3, // desativar registro
+        ]);
+
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>28,
+            'id_perfil_igreja_modulo'=>9, // pastor - Igreja 3 - Configurações
+            'id_permissao'=>1, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>29,
+            'id_perfil_igreja_modulo'=>9, // pastor - Igreja 3 - Configurações
+            'id_permissao'=>2, // alterar registro
+        ]);
+        DB::table('tbl_perfis_permissoes')->insert([
+            'id'=>30,
+            'id_perfil_igreja_modulo'=>9, // pastor - Igreja 3 - Configurações
+            'id_permissao'=>3, // desativar registro
         ]);
     }
 }
